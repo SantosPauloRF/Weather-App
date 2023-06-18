ee8a32d25939ab0ff45ee0654321ba1707caa7e3
Rafael Oliveira <rafael.silva@ebac.art.br>
2023-06-17 23:38:48 -0300

    making de url dynamic

--- main.js
+++ main.js
@@ -7,24 +7,31 @@
 //Retornar pelo log o resultado da requisição
 //Retornar pelo log apenas os graus c°
 
+// function ola() {
+//     const nomeA = "Rafael"
+//     const nomeB = "Paulo"
+//     //console.log (nomea + " " + nomeb)
+//     //console.log (`${nomeA} e ${nomeB}`)
+// }
 
-function weather() {
+// //ola()
+
+function weather(cidade) {
+  const apiKey = "5386ea7712884ed4b9c02055231806"
   const options = {
     headers: {
       mode: "cors",
     },
   }
-  fetch(
-    "http://api.weatherapi.com/v1/current.json?key=5386ea7712884ed4b9c02055231806&q=Santos", options)
+  fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cidade}`, options)
     .then((data) => {
       return data.json();
     })
     .then((result) => {
       console.log(result.location.name)
+      console.log(`${result.current.temp_c}°`)
     })
 }
 
-weather();
-
-
+weather("São Paulo");
 
