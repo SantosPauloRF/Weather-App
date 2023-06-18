f835e692cc86b847a5ea1cbc8794968020d21f65
Paulo Santos <santos.paulo.rf@gmail.com>
2023-06-17 21:59:03 -0300

    pseudoCode

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log("Hello world!");
+
+
+//Fazer umn função - retornar um console.log dizendo "dentro da função"
+
+//pesquisar como fazer requisição com js
+
+//fazer a requisição com js para o endpoind da api
+
+//Retornar pelo log o resultado da requisição
+
+//Retornar pelo log apenas os graus c°
