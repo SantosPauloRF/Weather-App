c1c93a5308f9b9e5c80570333bba0055f74a8356
Rafael-Oliveir4 <134177453+Rafael-Oliveir4@users.noreply.github.com>
2023-06-17 23:06:14 -0300

    Task02 (#2)
    * startint the requiring
    
    * update
    
    * requirements concluded

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
-console.log("Hello world!");
-
+// console.log("Hello world!");
 
 //Fazer umn função - retornar um console.log dizendo "dentro da função"
 
 //pesquisar como fazer requisição com js
-
 //fazer a requisição com js para o endpoind da api
-
 //Retornar pelo log o resultado da requisição
-
 //Retornar pelo log apenas os graus c°
+
+
+function weather() {
+  const options = {
+    headers: {
+      mode: "cors",
+    },
+  }
+  fetch(
+    "http://api.weatherapi.com/v1/current.json?key=5386ea7712884ed4b9c02055231806&q=Santos", options)
+    .then((data) => {
+      return data.json();
+    })
+    .then((result) => {
+      console.log(result.location.name)
+    })
+}
+
+weather();
+
+
+
