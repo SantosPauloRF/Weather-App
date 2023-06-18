8ea1e212d9adf0c82435d32414c20f385ba1d717
Rafael Oliveira <rafael.silva@ebac.art.br>
2023-06-17 22:34:09 -0300

    startint the requiring

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
-console.log("Hello world!");
+// console.log("Hello world!");
 
 
 //Fazer umn função - retornar um console.log dizendo "dentro da função"
 
-//pesquisar como fazer requisição com js
+function jbl() {
+   //pesquisar como fazer requisição com js
+   fetch("http://api.weatherapi.com/v1/search.json?key=5386ea7712884ed4b9c02055231806&q=sao")
+   .then(data => {
+    return data.json()
+    })
+    .then(post => {
+    console.log(post)
+    })
+}
+
+
+
+
+
 
 //fazer a requisição com js para o endpoind da api
 
