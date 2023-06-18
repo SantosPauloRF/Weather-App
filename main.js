c7b5bf95ffaa29b049aff5ddbf50bb8d66c65612
Rafael Oliveira <rafael.silva@ebac.art.br>
2023-06-17 22:47:33 -0300

    update

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
 // console.log("Hello world!");
 
-
 //Fazer umn função - retornar um console.log dizendo "dentro da função"
 
-function jbl() {
-   //pesquisar como fazer requisição com js
-   fetch("http://api.weatherapi.com/v1/search.json?key=5386ea7712884ed4b9c02055231806&q=sao")
-   .then(data => {
-    return data.json()
+function weather() {
+  const options = {
+    headers: {
+      mode: "cors",
+    },
+  };
+  //pesquisar como fazer requisição com js
+  fetch(
+    "http://api.weatherapi.com/v1/search.json?key=5386ea7712884ed4b9c02055231806&q=sao",
+    options
+  )
+    .then((data) => {
+      return data.json();
     })
-    .then(post => {
-    console.log(post)
-    })
+    .then((post) => {
+      console.log(post);
+    });
 }
 
-
-
-
-
+weather();
 
 //fazer a requisição com js para o endpoind da api
 
