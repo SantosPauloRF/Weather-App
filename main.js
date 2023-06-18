86295dac01eec0a3f04c88e15153773b4e13a8c5
Rafael Oliveira <rafael.silva@ebac.art.br>
2023-06-17 23:04:33 -0300

    requirements concluded

--- main.js
+++ main.js
@@ -2,29 +2,29 @@
 
 //Fazer umn função - retornar um console.log dizendo "dentro da função"
 
+//pesquisar como fazer requisição com js
+//fazer a requisição com js para o endpoind da api
+//Retornar pelo log o resultado da requisição
+//Retornar pelo log apenas os graus c°
+
+
 function weather() {
   const options = {
     headers: {
       mode: "cors",
     },
-  };
-  //pesquisar como fazer requisição com js
+  }
   fetch(
-    "http://api.weatherapi.com/v1/search.json?key=5386ea7712884ed4b9c02055231806&q=sao",
-    options
-  )
+    "http://api.weatherapi.com/v1/current.json?key=5386ea7712884ed4b9c02055231806&q=Santos", options)
     .then((data) => {
       return data.json();
     })
-    .then((post) => {
-      console.log(post);
-    });
+    .then((result) => {
+      console.log(result.location.name)
+    })
 }
 
 weather();
 
-//fazer a requisição com js para o endpoind da api
 
-//Retornar pelo log o resultado da requisição
 
-//Retornar pelo log apenas os graus c°
